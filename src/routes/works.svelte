4f89931ee88c7bba5cd1899d068166240f9b277d
huseyin <huseyin@keyko.io>
2022-07-21 01:55:31 +0300

    added works page

--- src/routes/works.svelte
+++ src/routes/works.svelte
@@ -1,1 +1,11 @@
-<div>works</div>
+<script>
+	import HomeSlot from '../components/HomeSlot.svelte';
+</script>
+
+<HomeSlot title="Projects">
+	<div class="grid grid-cols-1 mt-6 gap-6">
+		<img src="/wordle1.png" alt="" class="object-contain rounded-xl" />
+		<img src="/jobify1.png" alt="" class="object-contain rounded-xl" />
+		<img src="/twitter1.png" alt="" class="object-contain rounded-xl" />
+	</div>
+</HomeSlot>
